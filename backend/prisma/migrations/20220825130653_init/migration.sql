bf211450da1e9145586d6b9a6e0ff3f689f86539
thisisfaisalhere <thisisfaisalhere@gamil.com>
2022-08-25 18:52:12 +0530

    Init

---
+++ backend/prisma/migrations/20220825130653_init/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Post";
+
+-- CreateTable
+CREATE TABLE "companies" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "company_name" VARCHAR(255) NOT NULL,
+    "cin" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "companies_pkey" PRIMARY KEY ("id")
+);
